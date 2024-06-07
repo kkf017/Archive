5a5071f07cf79edf4895492bf7c718b7992941bd
kkf17 <favre.kathleen@orange.fr>
2024-06-07 12:07:53 +0200

    Updt project1

---
+++ Project1/Client-Server-C--main/Client-Server-2/src/protocol/protocol.h
@@ -0,0 +1,7 @@
+#ifndef PROTOCOL
+#define PROTOCOL 
+
+void send_(int sock, char* filename);
+void rcv_(int sock);
+
+#endif
---
+++ Project1/project/Example/unixcorn.py
@@ -0,0 +1,10 @@
+import random
+
+from typing import List
+
+def hello(x:str)->str:
+	return f"Hello, {x}."
+
+if __name__ == "__main__":
+	x = hello("U")
+	print(x)

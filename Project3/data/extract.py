778a838fb5151c4fa8a55908e82728363491bc3a
kkf17 <favre.kathleen@orange.fr>
2024-07-09 18:35:48 +0200

    Updt data

--- Project3/data/extract.py
+++ Project3/data/extract.py
@@ -1,11 +1,13 @@
 import os
+import copy
 from urllib.request import urlopen
 import bs4
 
 # pip install beautifulsoup4
+# pip install regex
 
 FOLDER = "/home/ksys/Documents/Project3/data/"
-EXCEPT = ["dordogne"]
+EXCEPT = ["dordogne", "https://www.boites-a-livres.fr/ville/aigre/16140", "https://www.boites-a-livres.fr/ville/aigre/16140","https://www.boites-a-livres.fr/ville/ailhon/07200", "https://www.boites-a-livres.fr/ville/magnieu/01300", "https://www.boites-a-livres.fr/ville/moulins/03000", "https://www.boites-a-livres.fr/ville/bazeilles/08140"]
 
 
 
@@ -49,30 +51,70 @@ def get_municipality(filename):
 
 def get_place(filename):
 	def link(filename):
-		with open(filename, 'r') as f:
-			for line in f.readlines():
-				print(line)
-				
-				page = urlopen(line)
-				html = page.read()
-				html = html.decode("utf-8")
-				print(html)
-				
-				# find address - write in .txt file
-				input()
+		def get_text_1(html):
+			soup = bs4.BeautifulSoup(html) 
+			for li in soup.findAll('ul'):
+				if not li == None:
+					for x in soup.findAll('li'):
+						if "Adresse approximative" in x.text:
+							return x.text.split(":")[1][1:]
+			return None
+		
+		def finditall(msg):	
+			def findit(sub):
+				first = sub.find('.bindPopup("<small><a href=')
+				last =  sub.find(">Voir la fiche détaillée</a></small>")
+				if first == -1:
+					return (None, None)
+				#print(sub[first+len('.bindPopup("<small><a href=')+1: last][:-1])
+				return (first+len('.bindPopup("<small><a href=')+1, last)
+			
+			res = []
+			while 1:
+				x, y = findit(msg)
+				if x == None and y == None:
+					break
+				res.append(msg[x:y][:-1])
+				msg = copy.copy(msg[y+len(">Voir la fiche détaillée</a></small>"):])
+			return res
+			
+		if True: #with open("{FOLDER}{filename}/{filename}.txt", 'w') as writer:	
+			with open(f"{FOLDER}{filename}/{filename}-link.txt", 'r') as f:
+				for line in f.readlines():
+					print("\n\n--->",line)
+					
+					if not line in EXCEPT:
+						page = urlopen(line)
+						html = page.read()
+						html = html.decode("utf-8")
+					
+						x = get_text_1(html)
+						if not x == None:
+							print(x)
+							#writer.write(x+"\n")
+						else:
+							soup = bs4.BeautifulSoup(html) 
+							for li in soup.findAll('script', {"type": "text/javascript"}):
+								res = finditall(li.text)
+								for i in res:
+									page = urlopen(i)
+									html = page.read()
+									html = html.decode("utf-8")
+									x = get_text_1(html)
+									print(x)
+									#writer.write(x+"\n")
 				
 				
 	with open(filename, 'r') as f:
 		for line in f.readlines():
-			print(f"\n\n\n{FOLDER}{line[:-1]}/{line[:-1]}-link.txt")
-			link(f"{FOLDER}{line[:-1]}/{line[:-1]}-link.txt")
+			print(f"\n\n\n************************************************************")
+			print(f"{FOLDER}{line[:-1]}/{line[:-1]}-link.txt")
+			print(f"************************************************************")
+			link(line[:-1]) #f"{FOLDER}{line[:-1]}/{line[:-1]}-link.txt")
+			input()
 	
 
 if __name__ == "__main__":
 
 	#get_municipality("deparments-link.txt")
 	get_place("deparments-name.txt")
-
-	#for each link in department.txt
-		# create folder
-		# create .txt
